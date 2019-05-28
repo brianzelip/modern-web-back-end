f7fd48e814359ddc9ac294fdb3ec7d6564222812
Brian Zelip <bzelip@gmail.com>
2019-05-28 16:06:03 -0400

    routes.js: Init modular router

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,7 @@
-// routes go here
+const express = require('express');
+
+const controller = require('../controllers/controllers.js');
+
+const router = express.Router();
+
+router.get('/', controller.homePage);
