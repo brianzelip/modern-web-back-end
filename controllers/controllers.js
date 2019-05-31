8332e0ac4eb44865b374f76c1ad8ecdf53ae518e
Brian Zelip <bzelip@gmail.com>
2019-05-31 10:27:25 -0400

    controllers.js: GET swapi data and res.send() it

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,21 @@
 // controllers control what happens when a user activates a given route
 
+const axios = require('axios');
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
 
-exports.swapi = (req, res) => {};
+exports.swapi = (req, res) => {
+  const url = 'https://swapi.co/api/';
+  axios
+    .get(`${url}planets/1/?format=json`)
+    .then(payload => payload.data)
+    .then(data => {
+      console.log('\n\n\nDATA is:::', data);
+      res.send(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
