b4000eec8ccadd5812c4221391b561c6c0f7b7be
Brian Zelip <bzelip@gmail.com>
2019-06-14 16:37:42 -0400

    controllers.js: Start new swapi controller

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,21 +1,31 @@
 // controllers control what happens when a user activates a given route
 
-const axios = require('axios');
+// const axios = require('axios');
+const fs = require('fs');
 
 exports.homePage = (req, res) => {
   res.render('index');
 };
 
+// exports.swapi = (req, res) => {
+//   const url = 'https://swapi.co/api/';
+//   const resource = req.params.resource;
+
+//   axios
+//     .get(`${url}${resource}/?format=json`)
+//     .then(payload => {
+//       res.send(payload.data);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// };
+
 exports.swapi = (req, res) => {
-  const url = 'https://swapi.co/api/';
   const resource = req.params.resource;
 
-  axios
-    .get(`${url}${resource}/?format=json`)
-    .then(payload => {
-      res.send(payload.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  const data = JSON.parse(fs.readFileSync(`../data/${resource}.json`, 'utf8'));
+  console.log('data is:::', data);
+
+  // res.send(data);
 };
