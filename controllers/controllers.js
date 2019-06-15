bd7d3ee24c7162f9113c97070acc17bf30ca8230
Brian Zelip <bzelip@gmail.com>
2019-06-15 11:26:19 -0400

    controllers: Rename new controller 'get', keep old swapi

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -7,23 +7,23 @@ exports.homePage = (req, res) => {
   res.render('index');
 };
 
-// exports.swapi = (req, res) => {
-//   const url = 'https://swapi.co/api/';
-//   const resource = req.params.resource;
-
-//   axios
-//     .get(`${url}${resource}/?format=json`)
-//     .then(payload => {
-//       res.send(payload.data);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// };
-
-exports.swapi = (req, res) => {
+exports.get = (req, res) => {
   const resource = req.params.resource;
   const filePath = `data/${resource}.json`;
   const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
   res.send(data);
+};
+
+exports.swapi = (req, res) => {
+  const url = 'https://swapi.co/api/';
+  const resource = req.params.resource;
+
+  axios
+    .get(`${url}${resource}/?format=json`)
+    .then(payload => {
+      res.send(payload.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 };
