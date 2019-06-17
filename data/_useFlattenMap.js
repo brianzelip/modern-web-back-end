c5a8223f49566c32b50b1d80e4608d551ffafbb0
Brian Zelip <bzelip@gmail.com>
2019-06-16 21:56:09 -0400

    _useFlattenMap.js, temp.js: Try to figure out the tooling for auto update but no dice yet

--- data/_useFlattenMap.js
+++ data/_useFlattenMap.js
@@ -1,20 +1,39 @@
-// Call this program on the command line, passing in:
-// 1. map file
-// 2. data filenames, from the same directory.
-
 const fs = require('fs');
 
-const resources = [
-  'films',
-  'people',
-  'planets',
-  'species',
-  'starships',
-  'vehicles'
-];
+const films = require('./films.json');
+const people = require('./people.json');
+const planets = require('./planets.json');
+const species = require('./species.json');
+const starships = require('./starships.json');
+const vehicles = require('./vehicles.json');
+const _MAP = require('./_FlattenMap.json');
+
+const oldData = {
+  films,
+  people,
+  planets,
+  species,
+  starships,
+  vehicles
+};
 
 function arrOfFiles(resourceName) {
-  return resources.filter(resource => resource !== resourceName);
+  return Object.keys(oldData).filter(resource => resource !== resourceName);
 }
 
-console.log(arrOfFiles('species'));
+_MAP.forEach(mapObj => {
+  const resource = Object.keys(mapObj)[0];
+  const filesToParse = arrOfFiles(resource);
+
+  filesToParse.forEach(fileName => {
+    const oldDataAsString = JSON.stringify(oldData[fileName], null, 2);
+    // console.log('\n\n\nmapObj.films!!::', mapObj.films);
+    // console.log('\n\n\nmapObj!!::', mapObj);
+    const newDataAsString = mapObj[resource].reduce((acc, item) => {
+      const re = new RegExp(`${item[0]}`, 'g');
+      return acc.replace(re, item[1]);
+    }, oldDataAsString);
+
+    fs.writeFileSync(`__${fileName}.json`, newDataAsString);
+  });
+});
