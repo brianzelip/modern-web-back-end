66638f500a79426d1c3050271045c1a851f97885
Brian Zelip <bzelip@gmail.com>
2019-07-25 13:14:52 -0400

    controllers: Simplify

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,13 +2,7 @@ exports.homePage = (req, res) => {
   res.render('index');
 };
 
-const myName = {
-  firstName: 'Brian',
-  lastName: 'Zelip',
-}
-
 exports.get = (req, res) => {
   const resource = req.params.resource;
-  res.send(`The resource parameter sent in the GET request was: ${resource}`)
-  // res.send(myName);
+  // res.send(`The resource parameter sent in the GET request was: ${resource}`)
 };
