ff6bc663923f33d055141c1f550def3a22de9af8
Brian Zelip <bzelip@gmail.com>
2019-07-25 10:42:58 -0400

    controllers, routes: Ready files for use

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,29 +1,4 @@
-// controllers control what happens when a user activates a given route
-
-const fs = require('fs');
-const axios = require('axios');
-
 exports.homePage = (req, res) => {
-  res.render('index');
-};
-
-exports.get = (req, res) => {
-  const resource = req.params.resource;
-  const filePath = `data/${resource}.json`;
-  const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-  res.send(data);
-};
-
-exports.swapi = (req, res) => {
-  const url = 'https://swapi.co/api/';
-  const resource = req.params.resource;
-
-  axios
-    .get(`${url}${resource}/?format=json`)
-    .then(payload => {
-      res.send(payload.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  res.send('Hello world!');
+  // res.render('index');
 };
--- routes/routes.js
+++ routes/routes.js
@@ -6,8 +6,4 @@ const router = express.Router();
 
 router.get('/', controller.homePage);
 
-router.get('/get/:resource', controller.get);
-
-router.get('/swapi/:resource', controller.swapi);
-
 module.exports = router;
